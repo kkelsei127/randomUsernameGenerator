25138a270aa0c8d1f76789af0a07c20b1442d05b
Kelsei Kyser <kkelsei127@yahoo.com>
2023-04-17 18:33:53 -0400

    state issues

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import {getRanName} from "../utils/helpers"
+import getRanName from "../utils/helpers"
 
 
 
 export default function Username() {
   
-  const [username, setUsername] = useState();
-
+  const [username, setUsername] = useState("");
+  
   return (
     <div className="App">
       <h1>Click the button to get a random username!</h1>
-      <button onClick={() => setUsername()}>Generate</button>
-      <div>{getRanName(username)}</div>
+      {/* <button onClick={() => setUsername(getRanName())}>Generate</button> */}
+      <div>{username}</div>
     </div>
   );
 }
