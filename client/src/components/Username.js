b0f72e3bea51f10e78f6f2a9b9e108475946e24a
Kelsei Kyser <kkelsei127@yahoo.com>
2023-04-17 22:02:52 -0400

    fixed hooks issue

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
-import getRanName from "../utils/helpers"
+import GetRanName from "../utils/helpers"
 
 
 
 export default function Username() {
-  
   const [username, setUsername] = useState("");
+  
+  function GetName() { 
+    const randomName = GetRanName();
+    console.log("hello")
+    console.log("randomName", randomName);
+    setUsername(randomName);
+  }
+
   
   return (
     <div className="App">
       <h1>Click the button to get a random username!</h1>
-      {/* <button onClick={() => setUsername(getRanName())}>Generate</button> */}
+      <button onClick={()=>GetName()}>Generate</button>
       <div>{username}</div>
     </div>
   );
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,24 +1,11 @@
-import { useQuery } from '@apollo/client';
+// import { useQuery } from '@apollo/client';
 import { QUERY_USERNAMES } from '../utils/queries';
 import UsernameList from '../components/UsernameList';
 
-const getRanName = () => {
-    const { loading, data } = useQuery(QUERY_USERNAMES);
-    const usernames = data?.usernames || [];
-    const name = usernames(Math.floor(Math.random() * 50));
-    return (
-    <div>
-        {loading ? (
-        <div>Loading...</div>
-        ) : (
-            <UsernameList
-            name={name}
-            title="Some Feed for Thought(s)..."
-          />
-
-        )}
-  </div>)
-    
+export default function GetRanName() {
+    // const { loading, data } = useQuery(QUERY_USERNAMES);
+    const usernames = [] 
+    const randomName = Math.floor(Math.random() * 10);
+    return randomName;
 };
 
-export default getRanName;
